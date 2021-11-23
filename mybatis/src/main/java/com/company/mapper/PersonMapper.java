245d862708cbe235adf30262c17589744cc154ed
사용자명 <jupiter12345@naver.com>
2021-11-23 18:14:41 +0900

    수정

---
+++ mybatis/src/main/java/com/company/mapper/PersonMapper.java
@@ -0,0 +1,25 @@
+package com.company.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+public interface PersonMapper {//personmapper가 dao 역할
+	//입력
+	
+	//BindingException: Parameter 'id' not found. Available 
+	// parameters are [arg1, arg0, param1, param2]
+//	@Insert("insert into person(id,name) Values(#{id},#{name})")
+//	
+//	@Select("select name from person where id=#{id}")  
+//	
+//	@Update("update person set name=#{name} where id=#{id}")
+//	
+	
+	//인터페이스 + xml 조합 : 메소드명과 xml 아이디가 일치해야함!!! 매우 중요!!
+	public int insertPerson(@Param("id") String id,@Param("name") String name);//param은 id와 name들어오면 무조건 들어와야함
+	public String selectPerson(String id); //PersonMapper.xml에서 만든 selectPerson과 동일해야함
+	public int updatePerson(@Param("id") String id,@Param("name") String name);
+	public int deletePerson(String id);
+}
--- simple_board_spring_jdbc/src/main/java/com/company/persistence/BoardDAO.java
+++ simple_board_spring_jdbc/src/main/java/com/company/persistence/BoardDAO.java
@@ -1,14 +1,6 @@
 package com.company.persistence;
 
-import static com.company.persistence.JdbcUtil.*;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
